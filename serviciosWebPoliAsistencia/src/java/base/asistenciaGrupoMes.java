29ebfe32137d2e4b5b6f235fb277616e74d71778
CalebBolanos <bolanos.c@hotmail.com>
2018-06-29 19:18:27 -0500

    asistencia de grupos corregido

--- serviciosWebPoliAsistencia/src/java/base/asistenciaGrupoMes.java
+++ serviciosWebPoliAsistencia/src/java/base/asistenciaGrupoMes.java
@@ -19,41 +19,49 @@ public class asistenciaGrupoMes {
             + "                                                <th class=\"mdl-data-table__cell--non-numeric\">Nombre</th>\n"
             + "                                                <th class=\"mdl-data-table__cell--non-numeric\">Dias Asistidos</th>\n"
             + "                                                <th class=\"mdl-data-table__cell--non-numeric\">Dias Faltados</th>\n"
+            + "                                                <th class=\"mdl-data-table__cell--non-numeric\">% de Asistencia</th>\n"
+            + "                                                <th class=\"mdl-data-table__cell--non-numeric\">% de Inasistencia</th>\n"
             + "                                            </tr>\n"
             + "                                        </thead>\n"
             + "                                        <tbody>";
-    private int numeroAlumnos, totalDias, alumnoAsistido;
-    private float totalAsistido, totalFaltado;
-    String falta;
+    //para sacar promedios de un alumno en un mes
+    //dias habiles de un mes = falta+asitencia
+    private int diasHabiles = 0;
+    private int falta = 0, asistencia = 0;
+    private float promedioAsistenciaIndividual = 0f, promedioInasistenciaIndividual = 0f;
+    //para sacar los promedios de un turno en un mes
+    private int numeroAlumnos = 0;
+    private float sumaAsistencia = 0f, sumaInasistencia = 0f;
+    private float promedioAsistencia = 0f, promedioInasistencia = 0f;
     public asistenciaGrupoMes(int mes, String grupo) {
         conexion.cDatos base = new conexion.cDatos();
         try {
             base.conectar();
             ResultSet resultado = base.consulta("call spAsistenciaGrupo("+mes+",'"+grupo+"');");
-            ResultSet dias = base.consulta("call spATotalGrupo("+mes+",'"+grupo+"');");
-            if(dias.next()){
-                totalDias = dias.getInt("asistencias");
-            }
             while (resultado.next()) {
-                
-                if(resultado.getString("faltados") == null){
-                    falta = "0";
-                }else{
-                    falta = resultado.getString("faltados");
-                }
+                asistencia = Integer.parseInt(resultado.getString("asistidos"));
+                falta = Integer.parseInt(resultado.getString("faltados"));
+                diasHabiles = asistencia + falta;
+                promedioInasistenciaIndividual = (falta * 100) / diasHabiles;
+                promedioAsistenciaIndividual = 100 - promedioInasistenciaIndividual;
                 html += " <tr>\n"
                         + "    <td class=\"mdl-data-table__cell--non-numeric\">" + resultado.getString("boleta") + "</td>\n"
                         + "    <td class=\"mdl-data-table__cell--non-numeric\">" + resultado.getString("nombre") + "</td>\n"
                         + "    <td class=\"mdl-data-table__cell--non-numeric\">" + resultado.getString("asistidos") + "</td>\n"
-                        + "    <td class=\"mdl-data-table__cell--non-numeric\">" + falta + "</td>\n"
+                        + "    <td class=\"mdl-data-table__cell--non-numeric\">" + resultado.getString("faltados") + "</td>\n"
+                        + "    <td class=\"mdl-data-table__cell--non-numeric\">" + promedioAsistenciaIndividual + "%" + "</td>\n"
+                        + "    <td class=\"mdl-data-table__cell--non-numeric\">" + promedioInasistenciaIndividual + "%" + "</td>\n"
                         + "</tr>";
-                alumnoAsistido = Integer.parseInt(resultado.getString("asistidos"));
-                totalAsistido += alumnoAsistido / (float)totalDias;
                 numeroAlumnos++;
+                sumaAsistencia += promedioAsistenciaIndividual;
+                sumaInasistencia += promedioInasistenciaIndividual;
             }
             html += "</tbody>";
             html += "</table>";
-            totalFaltado = numeroAlumnos - totalAsistido;
+            if(numeroAlumnos > 0){
+                promedioAsistencia = sumaAsistencia / numeroAlumnos;
+                promedioInasistencia = sumaInasistencia / numeroAlumnos;
+            }
         } catch (Exception error) {
             html = "";
         }
@@ -65,18 +73,23 @@ public class asistenciaGrupoMes {
     }
     
     public float promedioAsistido(){
-        return totalAsistido;
+        return promedioAsistencia;
     }
     
     public float pormedioFaltado(){
-        return totalFaltado;
+        return promedioInasistencia;
     }
     
     public int totalDias(){
-        return totalDias;
+        return diasHabiles;
     }
     
     public float grafTotalDias(){
-        return totalAsistido * (float)1.3;
+        if(promedioAsistencia > promedioInasistencia){
+            return promedioAsistencia * (float)1.3;
+        }
+        else{
+            return promedioInasistencia * (float)1.3;
+        }
     }
 }
